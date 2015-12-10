6986041a793a4740427b0b41e8ac8b29b0c7eea4
midspace <midspace@hotmail.com>
2015-12-10 19:37:14 +1100

    Fixed #2
    Added Server logger.

---
+++ midspace.SolarTracker/Data/Scripts/midspace.SolarTracker/SunSensorScript.cs
@@ -0,0 +1,130 @@
+﻿namespace midspace.SolarTracker
+{
+    using System;
+    using System.Timers;
+    using Sandbox.Common;
+    using Sandbox.Common.ObjectBuilders;
+    using Sandbox.ModAPI;
+
+    [Sandbox.Common.MySessionComponentDescriptor(Sandbox.Common.MyUpdateOrder.AfterSimulation)]
+    public class SunSensorScript : MySessionComponentBase
+    {
+        #region fields
+
+        private bool _isInitialized;
+        //private bool _isClientRegistered;
+        private bool _isServerRegistered;
+        private Timer _timerEvents;
+
+        public static SunSensorScript Instance;
+
+        public TextLogger ServerLogger = new TextLogger();
+        //public TextLogger ClientLogger = new TextLogger();
+
+        #endregion
+
+        #region attaching events and wiring up
+
+        public override void UpdateAfterSimulation()
+        {
+            Instance = this;
+
+            // This needs to wait until the MyAPIGateway.Session.Player is created, as running on a Dedicated server can cause issues.
+            // It would be nicer to just read a property that indicates this is a dedicated server, and simply return.
+            if (!_isInitialized && MyAPIGateway.Session != null && MyAPIGateway.Session.Player != null)
+            {
+                if (MyAPIGateway.Session.OnlineMode.Equals(MyOnlineModeEnum.OFFLINE)) // pretend single player instance is also server.
+                    InitServer();
+                if (!MyAPIGateway.Session.OnlineMode.Equals(MyOnlineModeEnum.OFFLINE) && MyAPIGateway.Multiplayer.IsServer && !MyAPIGateway.Utilities.IsDedicated)
+                    InitServer();
+                //InitClient();
+            }
+
+            // Dedicated Server.
+            if (!_isInitialized && MyAPIGateway.Utilities != null && MyAPIGateway.Multiplayer != null
+                && MyAPIGateway.Session != null && MyAPIGateway.Utilities.IsDedicated && MyAPIGateway.Multiplayer.IsServer)
+            {
+                InitServer();
+                return;
+            }
+
+            base.UpdateAfterSimulation();
+        }
+
+        //private void InitClient()
+        //{
+        //    _isInitialized = true; // Set this first to block any other calls from UpdateAfterSimulation().
+        //    _isClientRegistered = true;
+
+        //    ClientLogger.Init("SunSensorClient.Log"); // comment this out if logging is not required for the Client.
+        //    ClientLogger.Write("SunSensor Client Log Started");
+        //    if (ClientLogger.IsActive)
+        //        VRage.Utils.MyLog.Default.WriteLine(String.Format("##Mod## SunSensor Client Logging File: {0}", ClientLogger.LogFile));
+        //}
+
+        private void InitServer()
+        {
+            _isInitialized = true; // Set this first to block any other calls from UpdateAfterSimulation().
+            _isServerRegistered = true;
+            ServerLogger.Init("SunSensorServer.Log", false, 10); // comment this out if logging is not required for the Server.
+            ServerLogger.Write("SunSensor Server Log Started");
+            if (ServerLogger.IsActive)
+                VRage.Utils.MyLog.Default.WriteLine(String.Format("##Mod## SunSensor Server Logging File: {0}", ServerLogger.LogFile));
+
+            // start the timer last, as all data should be loaded before this point.
+            ServerLogger.Write("Attaching Event timer.");
+            _timerEvents = new Timer(5000);
+            _timerEvents.Elapsed += TimerEventsOnElapsed;
+            _timerEvents.Start();
+        }
+
+        #endregion
+
+        #region detaching events
+
+        protected override void UnloadData()
+        {
+            //if (_isClientRegistered)
+            //{
+            //    ClientLogger.Write("Closed");
+            //    ClientLogger.Terminate();
+            //}
+
+            if (_isServerRegistered)
+            {
+                if (_timerEvents != null)
+                {
+                    ServerLogger.Write("Stopping Event timer.");
+                    _timerEvents.Stop();
+                    _timerEvents.Elapsed -= TimerEventsOnElapsed;
+                    _timerEvents = null;
+                }
+
+                ServerLogger.Write("Closed");
+                ServerLogger.Terminate();
+            }
+
+            base.UnloadData();
+        }
+
+        #endregion
+
+        #region message handling
+
+        private void TimerEventsOnElapsed(object sender, ElapsedEventArgs elapsedEventArgs)
+        {
+            // DO NOT SET ANY IN GAME API CALLS HERE. AT ALL!
+            MyAPIGateway.Utilities.InvokeOnGameThread(delegate ()
+            {
+                // Recheck main Gateway properties, as the Game world my be currently shutting down when the InvokeOnGameThread is called.
+                if (MyAPIGateway.Players == null || MyAPIGateway.Entities == null || MyAPIGateway.Session == null || MyAPIGateway.Utilities == null)
+                    return;
+
+                // Any processing needs to occur in here, as it will be on the main thread, and hopefully thread safe.
+                // : here
+            });
+        }
+
+        #endregion
+    }
+}
---
+++ midspace.SolarTracker/Data/Scripts/midspace.SolarTracker/TextLogger.cs
@@ -0,0 +1,186 @@
+﻿namespace midspace.SolarTracker
+{
+    using System;
+    using System.IO;
+    using Sandbox.ModAPI;
+
+    public class TextLogger
+    {
+        #region fields
+
+        private string _logFileName;
+        private TextWriter _logWriter;
+        private bool _isInitialized;
+        private int _delayedWrite;
+        private int _writeCounter;
+
+        #endregion
+
+        #region properties
+
+        public string LogFileName { get { return _logFileName; } }
+
+        public string LogFile { get { return Path.Combine(MyAPIGateway.Utilities.GamePaths.UserDataPath, "Storage", _logFileName); } }
+
+        public bool IsActive { get { return _isInitialized; } }
+
+        #endregion
+
+        #region ctor
+
+        /// <summary>
+        /// Initialize the TextLogger with a default filename.
+        /// The TextLogger must be Initialized before it can write log entries.
+        /// This allows a TextLogger to be created and the Write(...) methods invoked without the TextLogger initialized so you don't have to wrap the TextLogger variable with if statements.
+        /// </summary>
+        public void Init()
+        {
+            _isInitialized = true;
+            _logFileName = string.Format("TextLog_{0}_{1:yyyy-MM-dd_HH-mm-ss}.log", MyAPIGateway.Session != null ? Path.GetFileNameWithoutExtension(MyAPIGateway.Session.CurrentPath) : "0", DateTime.Now);
+        }
+
+        /// <summary>
+        /// Initialize the TextLogger with a custom filename.
+        /// The TextLogger must be Initialized before it can write log entries.
+        /// This allows a TextLogger to be created and the Write(...) methods invoked without the TextLogger initialized so you don't have to wrap the TextLogger variable with if statements.
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="addTimestamp"></param>
+        /// <param name="delayedWrite"></param>
+        public void Init(string filename, bool addTimestamp = false, int delayedWrite = 0)
+        {
+            _isInitialized = true;
+            if (addTimestamp)
+                _logFileName = string.Format("TextLog_{0}_{1:yyyy-MM-dd_HH-mm-ss}{2}", Path.GetFileNameWithoutExtension(filename), DateTime.Now, Path.GetExtension(filename));
+            else
+                _logFileName = filename;
+
+            _delayedWrite = delayedWrite;
+        }
+
+        ~TextLogger()
+        {
+            Terminate();
+        }
+
+        #endregion
+
+        public void Write(string text, params object[] args)
+        {
+            if (!_isInitialized)
+                return;
+
+            // we create the writer when it is needed to prevent the creation of empty files
+            if (_logWriter == null)
+            {
+                try
+                {
+                    _logWriter = MyAPIGateway.Utilities.WriteFileInGlobalStorage(_logFileName);
+                }
+                catch (Exception ex)
+                {
+                    Terminate();
+                    WriteGameLog("## TextLogger Exception caught in mod. Message: {0}", ex.Message);
+                    return;
+                }
+            }
+
+            string message;
+            if (args == null || args.Length == 0)
+                message = text;
+            else
+                message = string.Format(text, args);
+
+            _logWriter.WriteLine("{0:yyyy-MM-dd HH:mm:ss:fff} - {1}", DateTime.Now, message);
+            _writeCounter++;
+            if (_delayedWrite == 0 || _writeCounter > _delayedWrite)
+            {
+                _logWriter.Flush();
+                _writeCounter = 0;
+            }
+        }
+
+        public void WriteRaw(string text, params object[] args)
+        {
+            if (!_isInitialized)
+                return;
+
+            // we create the writer when it is needed to prevent the creation of empty files
+            if (_logWriter == null)
+            {
+                try
+                {
+                    _logWriter = MyAPIGateway.Utilities.WriteFileInGlobalStorage(_logFileName);
+                }
+                catch (Exception ex)
+                {
+                    Terminate();
+                    WriteGameLog("## TextLogger Exception caught in mod. Message: {0}", ex.Message);
+                    return;
+                }
+            }
+
+            string message;
+            if (args == null || args.Length == 0)
+                message = text;
+            else
+                message = string.Format(text, args);
+
+            _logWriter.Write(message);
+            _writeCounter++;
+            if (_delayedWrite == 0 || _writeCounter > _delayedWrite)
+            {
+                _logWriter.Flush();
+                _writeCounter = 0;
+            }
+        }
+
+        public void WriteException(Exception ex, string additionalInformation = null)
+        {
+            if (!_isInitialized)
+                return;
+
+            // we create the writer when it is needed to prevent the creation of empty files
+            if (_logWriter == null)
+                _logWriter = MyAPIGateway.Utilities.WriteFileInGlobalStorage(_logFileName);
+
+            _logWriter.WriteLine("{0:yyyy-MM-dd HH:mm:ss:fff} Error - {1}", DateTime.Now, ex);
+
+            if (!string.IsNullOrEmpty(additionalInformation))
+            {
+                _logWriter.WriteLine("Additional information on {0}:", ex.Message);
+                _logWriter.WriteLine(additionalInformation);
+            }
+
+            _writeCounter++;
+            if (_delayedWrite == 0 || _writeCounter > _delayedWrite)
+            {
+                _logWriter.Flush();
+                _writeCounter = 0;
+            }
+        }
+
+        public void Terminate()
+        {
+            _isInitialized = false;
+            if (_logWriter != null)
+            {
+                _logWriter.Flush();
+                _logWriter.Close();
+                _logWriter = null;
+            }
+        }
+
+        public static void WriteGameLog(string text, params object[] args)
+        {
+            string message = text;
+            if (args != null && args.Length != 0)
+                message = string.Format(text, args);
+
+            if (MyAPIGateway.Utilities.IsDedicated)
+                VRage.Utils.MyLog.Default.WriteLineAndConsole(message);
+            else
+                VRage.Utils.MyLog.Default.WriteLine(message);
+        }
+    }
+}
